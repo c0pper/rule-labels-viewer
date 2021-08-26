830b69b35544328ceb21e773e1e14bab9aa6595f
smarotta <smarotta@expert.ai>
2021-08-26 17:33:32 +0200

    deleted multiple entries

--- project_details.py
+++ project_details.py
@@ -2,20 +2,6 @@ karman_line = {
     "cat": "Blog",
     "date": "2020",
     "url": "http://thekarmanline.atwebpages.com/",
-    "desc": "A blog about space related news. All the articles are translated from Japanese sources as a way to practice language and translation."
-}
-
-karman_line = {
-    "cat": "Blog",
-    "date": "2020",
-    "url": "http://thekarmanline.atwebpages.com/",
-    "desc": "A blog about space related news. All the articles are translated from Japanese sources as a way to practice language and translation."
-}
-
-karman_line = {
-    "cat": "Blog",
-    "date": "2020",
-    "url": "http://thekarmanline.atwebpages.com/",
     "desc": "A blog about space related news. All the articles are translated from Japanese sources as a way to practice language and translation.",
     "img1": "portfolio-details-1.jpg",
     "img2": "portfolio-details-2.jpg",
