f806368adb79ca7c74e910f3a1013a8db3d3db1b
smarotta <smarotta@expert.ai>
2021-08-27 09:47:26 +0200

    add resume.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
 from datetime import date
+from resume import ed_experiences, w_experiences
 from project_details import *
+
 
 app = Flask(__name__)
 
@@ -12,24 +14,27 @@ email = "martin.s.marotta@gmail.com"
 location = "Naples, IT"
 skype = "martin.s.marotta@gmail.com"
 
-navmenu = [("#hero", "bx bx-home", "Home"),
-("#about", "bx bx-user", "About"),
-("#skills", "bx bx-user", "Skills"),
-("#resume", "bx bx-file-blank", "Resume"),
-("#projects", "bx bx-book-content", "Projects"),
-("#contact", "bx bx-envelope", "Contact")]
+navmenu = [
+    ("#hero", "bx bx-home", "Home"),
+    ("#about", "bx bx-user", "About"),
+    ("#skills", "bx bx-user", "Skills"),
+    ("#resume", "bx bx-file-blank", "Resume"),
+    ("#projects", "bx bx-book-content", "Projects"),
+    ("#contact", "bx bx-envelope", "Contact")
+]
 
 lang_skills = [("english", 100), ("Russian", 70), ("SPANISH", 30), ("FRENCH", 30), ("JAPANESE", 20)]
 comp_skills = [("PHOTOSHOP", 80), ("HTML", 70), ("PYTHON", 50), ("WORDPRESS/CMS", 50), ("CSS", 30)]
 
-projects = [("The Karman Line", "karman.png", "karman-line"),
-("This website. Like it's '95.", "win95.png", "win95-site"),
-("SpaceX Website italian localisation", "spacex.png", "spacex-it"),
-("GTA Napoli", "gta.png", "gta-napoli"),
-("Unidia", "unidia.png", "unidia"),
-("'Mirai' Substitles", "mirai.png", "mirai-subs"),
-("'Clients' localisation", "clients.png", "clients-app"),
-("'NMusic' localisation", "nmusic.png", "nmusic-app")
+projects = [
+    ("The Karman Line", "karman.png", "karman-line"),
+    ("This website. Like it's '95.", "win95.png", "win95-site"),
+    ("SpaceX Website italian localisation", "spacex.png", "spacex-it"),
+    ("GTA Napoli", "gta.png", "gta-napoli"),
+    ("Unidia", "unidia.png", "unidia"),
+    ("'Mirai' Substitles", "mirai.png", "mirai-subs"),
+    ("'Clients' localisation", "clients.png", "clients-app"),
+    ("'NMusic' localisation", "nmusic.png", "nmusic-app")
 ]
  
 
@@ -42,90 +47,101 @@ def calculateAge(birthDate):
 
 @app.route("/")
 def home():
-    return render_template("index.html",
-    title=title,
-    navmenu=navmenu,
-    email=email,
-    location=location,
-    skype=skype,
-    age=calculateAge(date(1993, 9, 4)),
-    copyright_year=current_year,
-    lang_skills=lang_skills,
-    comp_skills=comp_skills,
-    projects=projects
+    return render_template(
+        "index.html",
+        title=title,
+        navmenu=navmenu,
+        email=email,
+        location=location,
+        skype=skype,
+        age=calculateAge(date(1993, 9, 4)),
+        copyright_year=current_year,
+        lang_skills=lang_skills,
+        comp_skills=comp_skills,
+        ed_experiences=ed_experiences,
+        w_experiences=w_experiences,
+        projects=projects
     )
 
 
 @app.route("/karman-line")
 def karman():
-    return render_template("karman-line.html",
-    title="The Karman Line",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=karman_line
+    return render_template(
+        "karman-line.html",
+        title="The Karman Line",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=karman_line
     )
 
 @app.route("/win95-site")
 def win95():
-    return render_template("win95-site.html",
-    title="This website Windows 95ed",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=win95_site
+    return render_template(
+        "win95-site.html",
+        title="This website Windows 95ed",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=win95_site
     )
 
 @app.route("/spacex-it")
 def spacex():
-    return render_template("spacex-it.html",
-    title="SpaceX Italian Localisation",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=spacex_it
+    return render_template(
+        "spacex-it.html",
+        title="SpaceX Italian Localisation",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=spacex_it
     )
 
 @app.route("/gta-napoli")
 def gta():
-    return render_template("gta-napoli.html",
-    title="GTA Napoli",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=gta_napoli
+    return render_template(
+        "gta-napoli.html",
+        title="GTA Napoli",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=gta_napoli
     )
 
 @app.route("/unidia")
 def uni():
-    return render_template("unidia.html",
-    title="Unidia",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=unidia
+    return render_template(
+        "unidia.html",
+        title="Unidia",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=unidia
     )
 
 @app.route("/mirai-subs")
 def mirai():
-    return render_template("mirai-subs.html",
-    title="Mirai Subtitles",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=mirai_subs
+    return render_template(
+        "mirai-subs.html",
+        title="Mirai Subtitles",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=mirai_subs
     )
 
 @app.route("/clients-app")
 def clients():
-    return render_template("clients-app.html",
-    title="Clients App Localisation",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=clients_app
+    return render_template(
+        "clients-app.html",
+        title="Clients App Localisation",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=clients_app
     )
 
 @app.route("/nmusic-app")
 def nmusic():
-    return render_template("nmusic-app.html",
-    title="NMusic App Localisation",
-    copyright_year=current_year,
-    navmenu=navmenu,
-    data=nmusic_app
+    return render_template(
+        "nmusic-app.html",
+        title="NMusic App Localisation",
+        copyright_year=current_year,
+        navmenu=navmenu,
+        data=nmusic_app
     )
 
 
---
+++ resume.py
@@ -0,0 +1,149 @@
+ed_experiences = [
+    {
+        "name": "MASTER'S DEGREE - TECHNICAL TRANSLATION",
+        "date": "2017 - 2020",
+        "institution": "Università degli Studi di Naples L’Orientale",
+        "location": ", Naples (IT)",
+        "points": [
+            "Graduated 110/110",
+            "Skills in specialized and professional texts translation",
+            "Awareness of phenomena related to interaction of cultures",
+            "Optimization of work through the use of IT resources"
+        ],
+        "url": "https://www.unior.it/"
+    },
+    {
+        "name": "APPLE IOS FOUNDATION PROGRAM",
+        "date": "2018",
+        "institution": "Università degli Studi di Naples L’Orientale",
+        "location": ", Naples (IT)",
+        "points": [
+            "First place in admission list",
+            "Developement of publishable app in 1 month (Swift)",
+            "Demo of the product at European Researchers’ Night ‘19, national science fair “Futuro Remoto” and Universirty of Westminster (London)",
+            "Honing of soft / hard skills and team working"
+        ],
+        "url": "https://www.unior.it/"
+    },
+    {
+        "name": "FIT - INITIAL FORMATION AND TRAINEESHIP FOR TEACHERS",
+        "date": "2018",
+        "institution": "Università degli Studi di Naples L’Orientale",
+        "location": ", Naples (IT)",
+        "points": [
+            "Graduated 102/110",
+            "Promotion of activities related to multilingualism and interculturalism",
+            "Teaching language learning techniques",
+            "Digital texts localization and CAT tools"
+        ],
+        "url": "https://www.unior.it/"
+    },
+    {
+        "name": "BA - LINGUISTIC AND CULTURAL MEDIATION",
+        "date": "2012 - 2016",
+        "institution": "Università degli Studi di Naples L’Orientale",
+        "location": ", Naples (IT)",
+        "points": [
+            "Graduated 102/110",
+            "Promotion of activities related to multilingualism and interculturalism",
+            "Teaching language learning techniques",
+            "Digital texts localization and CAT tools"
+        ],
+        "url": "https://www.unior.it/"
+    }
+]
+w_experiences = [
+    {
+        "name": "KNOWLEDGE ENGINEER",
+        "date": "2021 - Present",
+        "institution": "Expert.AI",
+        "location": ", Naples (IT)",
+        "points": [
+            "Development of semantic rules that provide the best results in order to meet customer expectations",
+            "Identification of logical patterns underlying the language",
+            "Improvement of the basic technology from a linguistic point of view",
+            "Tests and validations to verify the effectiveness of the implemented solution",
+            "Identification of suitable solutions for clients in the field of Natural Language Processing"
+        ],
+        "url": "https://www.expert.ai/"
+    },
+    {
+        "name": "ENGLISH TEACHER",
+        "date": "2021",
+        "institution": "Istituto Scolastico Sant'Agostino",
+        "location": ", Aversa (IT)",
+        "points": [
+            "Planning and delivering English lessons for kindergarten learners",
+            "Utilising classroom management techniques to improve efficiency of lessons",
+            "Enriching the lesson by including additional audio-visual material"
+        ],
+        "url": "https://www.santagostinoaversa.it/"
+    },
+    {
+        "name": "MUSIC VIDEO TECHNICAL ASSISTANT",
+        "date": "2020",
+        "institution": "Stre_musica",
+        "location": "",
+        "points": [
+            "Creation of animated clips from still photos using AI",
+            "Technical consultation and editing assistance"
+        ],
+        "url": "https://www.youtube.com/watch?v=0a0jhmPL-Io"
+    },
+    {
+        "name": "FREELANCE TRANSLATOR",
+        "date": "2019",
+        "institution": "Upwork",
+        "location": "",
+        "points": [
+            "Italian localisation of Clients app for iOS",
+            "Italian localisation of N Music for Android"
+        ],
+        "url": "https://www.upwork.com/freelancers/~010e7ee2cc3d745197"
+    },
+    {
+        "name": "ACTIVITY LEADER",
+        "date": "2019",
+        "institution": "St. Giles Juniors",
+        "location": ", Bournemouth (UK)",
+        "points": [
+            "Welcoming students and orientation",
+            "Monitoring welfare and discipline",
+            "Leading activities and groups during excursions",
+            "Encouraging engagement in social programme",
+            "Supervising arrivals/departures"
+        ],
+        "url": "https://www.stgiles-international.com/"
+    },
+    {
+        "name": "ACADEMIC INTERN",
+        "date": "2018",
+        "institution": "Brighton Language College",
+        "location": ", Brighton (UK)",
+        "points": [
+            "Teacher assistance",
+            "Groups coordination",
+            "Orientation",
+            "Cultural mediation",
+            "Excursions guide",
+            "Children supervision",
+            "Lesson planning",
+            "Attendance keeping",
+            "Database updating"
+        ],
+        "url": "https://www.stgiles-international.com/"
+    },
+    {
+        "name": "RECEPTION DESK",
+        "date": "2015",
+        "institution": "Naples Immigration Office",
+        "location": ", Naples (IT)",
+        "points": [
+            "Issuing residence permit to non-EU citizens",
+            "Cultural mediation",
+            "Database updating",
+            "Reception"
+        ],
+        "url": "http://www.prefettura.it/napoli/contenuti/Area_iv_immigrazione-419618.htm"
+    }
+]
