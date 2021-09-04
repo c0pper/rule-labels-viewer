60932c80485d7511f3e578ae60a75cf360a70fa0
Simo <93simonster@gmail.com>
2021-09-04 15:51:17 +0200

    random warnings fix

--- resume.py
+++ resume.py
@@ -19,8 +19,8 @@ ed_experiences = [
         "location": ", Naples (IT)",
         "points": [
             "First place in admission list",
-            "Developement of publishable app in 1 month (Swift)",
-            "Demo of the product at European Researchers' Night '19, national science fair 'Futuro Remoto' and Universirty of Westminster (London)",
+            "Development of publishable app in 1 month (Swift)",
+            "Demo of the product at European Researchers' Night '19, national science fair 'Futuro Remoto' and University of Westminster (London)",
             "Honing of soft / hard skills and team working"
         ],
         "url": "https://www.unior.it/"
@@ -31,7 +31,7 @@ ed_experiences = [
         "institution": "Università degli Studi di Naples L'Orientale",
         "location": ", Naples (IT)",
         "points": [
-            "Antropology",
+            "Anthropology",
             "Pedagogy",
             "Teaching to migrants",
             "Teaching to people with special needs"
@@ -64,7 +64,7 @@ w_experiences = [
             "Improvement of the basic technology from a linguistic point of view",
             "Tests and validations to verify the effectiveness of the implemented solution",
             "Identification of suitable solutions for clients in the field of Natural Language Processing",
-            "App mockup deisgn and creation"
+            "App mockup design and creation"
         ],
         "url": "https://www.expert.ai/"
     },
