16caee18b5d4e028a11894e1263469d2dba7639f
93simon <93simonster@gmail.com>
2022-10-04 16:58:45 +0200

    rules label list

---
+++ parser.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import re
+
+
+p = Path(r'C:\workspace\mirc0_trascrizioni\rules')
+rule_files = list(p.glob('**/*.cr'))
+
+for f in rule_files:
+    with open(f, "r", encoding="UTF8") as file:
+        content = file.read()
+        labels_list = re.findall(r'DOMAIN\[(.*?)\]', content)
+        print(labels_list, f.name)
